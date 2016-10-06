627833b3a3dfedf20b4a8ec4a8e31384f3ea8685
yoggy <yoggy0@gmail.com>
2016-10-06 22:28:32 +0900

    ...

--- max/mxj.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\Java\jdk1.8.0_45\bin\javac.exe" -classpath "C:\Program Files (x86)\Cycling '74\Max 7\resources\java-classes\lib\max.jar" mxjcls.java
---
+++ max/mxj/classes/mxj.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk1.8.0_102\bin\javac.exe" -classpath "C:\Program Files (x86)\Cycling '74\Max 7\resources\packages\max-mxj\java-classes\lib\max.jar" mxjcls.java
